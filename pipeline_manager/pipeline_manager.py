1e88ece8a0db64e40ce1d3cd493dc4c901c2aa8d
Taya Medvedeva <71186576+MTasia@users.noreply.github.com>
2020-10-11 21:06:15 +0300

    Add files via upload

--- pipeline_manager/pipeline_manager.py
+++ pipeline_manager/pipeline_manager.py
@@ -1,4 +1,42 @@
 def pipeline():
-    """
-    :return: returns a decorator that allows to add steps to the pipeline
-    """
+    graph = {}
+    function = {}
+    def decorator(depends_on=None):
+        if depends_on is None:
+            depends_on = []
+        def register(func):
+            graph.update({func.__name__: depends_on})
+            def inner():
+                for point in graph[func.__name__]:
+                    function[point]()
+                return func()
+            function.update({func.__name__: inner})
+            return inner
+        if callable(depends_on):
+            return decorator()(depends_on)
+        return register
+    return decorator
+
+
+# def test_example() -> None:
+#     register = pipeline()
+#
+#     answer = []
+#
+#     @register
+#     def step1():
+#         answer.append(1)
+#
+#     @register(["step1"])
+#     def step2():
+#         answer.append(2)
+#
+#     @register(["step1", "step2"])
+#     def step3():
+#         answer.append(3)
+#
+#     step3()
+#     print(answer)
+#
+#
+# test_example()
